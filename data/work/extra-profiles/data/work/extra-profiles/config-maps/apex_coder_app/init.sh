a83afe6e21f6f75f043cc635cff03f206cf0e9bb
pmembari <parham.membari@terradue.com>
2026-04-01 19:15:24 +0200

    fix: fix the deployment of apphub by changing chart index
    ESAAPEX-61

---
+++ data/work/extra-profiles/data/work/extra-profiles/config-maps/apex_coder_app/init.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+set -x 
+
+cd /workspace
+
+# -------------------------
+# Clone repositories
+# -------------------------
+rm -rf eoap openeo
+mkdir -p eoap
+cd eoap
+git clone https://github.com/eoap/cwl-eoap.git
+git clone https://github.com/eoap/stac-eoap.git
+git clone https://github.com/eoap/quickwin.git
+
+cd /workspace
+mkdir -p openeo
+cd openeo
+git clone --no-checkout https://github.com/Open-EO/openeo-community-examples.git
+cd openeo-community-examples
+git sparse-checkout init --cone
+git sparse-checkout set \
+  "python/1. GettingStarted/GettingStarted.ipynb" \
+  "python/LoadStac/load-stac-item-example.ipynb" \
+  "python/BioPAR/biopar_service.ipynb"
+git checkout main
+git checkout 738d1710918ad937c2d79c49436c6e7ca2abd3dd -- \
+  python/BioPAR/biopar_service.ipynb
+
+# -------------------------
+# VS Code extensions
+# -------------------------
+# Install VS Code Extensions
+code-server --install-extension ms-python.python 
+code-server --install-extension redhat.vscode-yaml
+code-server --install-extension sbg-rabix.benten-cwl
+code-server --install-extension ms-toolsai.jupyter
+ln -s /workspace/.local/share/code-server/extensions /workspace/extensions
+
+# Setup user settings
+mkdir -p /workspace/User/
+echo '{"workbench.colorTheme": "Visual Studio Dark"}' > /workspace/User/settings.json
+echo '[
+            {
+                "key": "ctrl+c",
+                "command": "workbench.action.terminal.copySelection",
+                "when": "terminalFocus && terminalTextSelected"
+            },
+            {
+                "key": "ctrl+v",
+                "command": "workbench.action.terminal.paste",
+                "when": "terminalFocus"
+            }
+            ]' > /workspace/User/keybindings.json
+
+# -------------------------
+# Python virtual environment
+# -------------------------
+python -m venv /workspace/.venv
+. /workspace/.venv/bin/activate
+
+# Upgrade pip and install minimal packages
+chmod -R 777 /workspace/.ipython
+/workspace/.venv/bin/python -m pip install --upgrade pip
+python -m pip install --no-cache-dir \
+  openeo \
+  click \
+  asyncclick \
+  click-logging \
+  requests \
+  numpy \
+  tqdm \
+  tabulate \
+  ipython \
+  ipykernel \
+  loguru \
+  shapely \
+  pyproj \
+  rasterio \
+  scikit-image \
+  pystac \
+  pystac-client \
+  stactools \
+  stactools-sentinel2 \
+  stac-asset \
+  rio-stac \
+  boto3 \
+  graphviz \
+  yq \
+  cwl-utils \
+  cwl-loader \
+  eoap-cwlwrap \
+  mkdocs-material \
+  mkdocs-mermaid2-plugin \
+  anyio \
+  matplotlib
+/workspace/.venv/bin/python -m pip install --no-cache-dir cwltool==3.1.20241112140730 
+
+# -------------------------
+# Register Jupyter kernel
+# -------------------------
+
+/workspace/.venv/bin/python -m ipykernel install --name openeo-env --display-name "OpenEO" --sys-prefix
+# -------------------------
+# Permissions (safe)
+# -------------------------
+mkdir -p /workspace/.podman
+chmod -R 777 /workspace/.local /workspace/.venv /workspace/User \
+    /workspace/eoap /workspace/openeo /workspace/.podman
+
+## AWS environment variables
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_ENDPOINT_URL=http://localstack:4566
+
+aws s3 mb s3://results --endpoint-url=${AWS_ENDPOINT_URL} || true
---
+++ data/work/extra-profiles/data/work/extra-profiles/config-maps/apex_jupyter_lab_app/init.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+set -x 
+
+cd /workspace
+# -------------------------
+# Clone repositories
+# -------------------------
+rm -rf eoap openeo
+mkdir -p eoap
+cd eoap
+git clone https://github.com/eoap/cwl-eoap.git
+git clone https://github.com/eoap/stac-eoap.git
+git clone https://github.com/eoap/quickwin.git
+cd /workspace
+mkdir -p openeo
+cd openeo
+git clone --no-checkout https://github.com/Open-EO/openeo-community-examples.git
+cd openeo-community-examples
+git sparse-checkout init --cone
+git sparse-checkout set \
+  "python/1. GettingStarted/GettingStarted.ipynb" \
+  "python/LoadStac/load-stac-item-example.ipynb" \
+  "python/BioPAR/biopar_service.ipynb"
+git checkout main
+git checkout 738d1710918ad937c2d79c49436c6e7ca2abd3dd -- \
+  python/BioPAR/biopar_service.ipynb
+
+
+cd /workspace
+# -------------------------
+# Python virtual environment
+# -------------------------
+python -m venv /workspace/.venv
+. /workspace/.venv/bin/activate
+
+/workspace/.venv/bin/python -m pip install --no-cache-dir \
+  openeo \
+  click \
+  asyncclick \
+  click-logging \
+  requests \
+  numpy \
+  tqdm \
+  tabulate \
+  ipython \
+  ipykernel \
+  loguru \
+  shapely \
+  pyproj \
+  rasterio \
+  scikit-image \
+  pystac \
+  pystac-client \
+  stactools \
+  stactools-sentinel2 \
+  stac-asset \
+  rio-stac \
+  boto3 \
+  graphviz \
+  yq \
+  cwl-utils \
+  cwl-loader \
+  eoap-cwlwrap \
+  mkdocs-material \
+  mkdocs-mermaid2-plugin \
+  anyio \
+  matplotlib
+
+/workspace/.venv/bin/python -m pip install --no-cache-dir cwltool==3.1.20241112140730 ipykernel 
+# -------------------------
+# Register Jupyter kernel
+# -------------------------
+/workspace/.venv/bin/python -m ipykernel install \
+    --prefix=/workspace/.local \
+    --name OpenEO \
+    --display-name "OpenEO"
+
+
+# -------------------------
+# Env
+# -------------------------
+chmod -R 777 /workspace/.local
+chmod -R 777 /workspace/eoap/
+chmod -R 777 /workspace/openeo/
+mkdir -p /workspace/.podman
+chmod -R 777 /workspace/.venv/
+chown -R 1001:100 /workspace/.podman
+chmod -R 775 /workspace/.podman
+
+export PATH=$PATH:/workspace/.venv/bin
+## AWS environment variables
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_ENDPOINT_URL=http://localstack:4566
+
+export AWS_ENDPOINT_URL=http://localstack:4566
+
+aws s3 mb s3://results --endpoint-url=${AWS_ENDPOINT_URL} || true
