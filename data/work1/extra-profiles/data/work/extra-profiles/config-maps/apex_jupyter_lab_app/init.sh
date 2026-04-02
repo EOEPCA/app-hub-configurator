65bedf7aa69ad86103fdcf79c92e463e1da4946a
pmembari <parham.membari@terradue.com>
2026-04-02 09:58:02 +0200

    feat: change the structure and add correct env vars
    ESAAPEX-61

--- data/work1/extra-profiles/data/work/extra-profiles/config-maps/apex_jupyter_lab_app/init.sh
+++ data/work1/extra-profiles/data/work/extra-profiles/config-maps/apex_jupyter_lab_app/init.sh
@@ -92,8 +92,8 @@ export PATH=$PATH:/workspace/.venv/bin
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
 export AWS_DEFAULT_REGION=us-east-1
-export AWS_ENDPOINT_URL=http://localstack:4566
+export AWS_ENDPOINT_URL=http://seaweedfs-s3:8333
 
-export AWS_ENDPOINT_URL=http://localstack:4566
+export AWS_ENDPOINT_URL=http://seaweedfs-s3:8333
 
 aws s3 mb s3://results --endpoint-url=${AWS_ENDPOINT_URL} || true
